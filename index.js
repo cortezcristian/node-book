897047859f890555580d1052948b7f5e1b6a785a
Cristian Cortez <cortez.cristian@gmail.com>
2018-10-26 23:58:37 -0300

    Opening the notebook on local

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const opener = require('opener');
+const express = require('express');
+
+const host = '127.0.0.1';
+const port = 3333;
+
+const app = express();
+
+app.listen(port, host, () => {
+  console.log(`Nodebooks: Started on ${host}:${port}`);
+  opener(`http://${host}:${port}`);
+});
+
